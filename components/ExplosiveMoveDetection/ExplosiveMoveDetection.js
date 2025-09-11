38a8d3502c2fe8e03361206c1e41e499fc443314
parag <pkakabari22@gmail.com>
2025-09-11 23:49:57 +0530

    Fix typo in welcome message in ExplosiveMoveDetection component

--- components/ExplosiveMoveDetection/ExplosiveMoveDetection.js
+++ components/ExplosiveMoveDetection/ExplosiveMoveDetection.js
@@ -23,7 +23,7 @@ export default function ExplosiveMoveDetection() {
 
             <div className='explosice-move-detection-alignment'>
                 <div className='welcome-line'>
-                    <p>Welcome to CoinPulse1</p>
+                    <p>Welcome to CoinPulse</p>
 
                     <h1 className={`${isDarkMode ? 'dark' : ''}`}>Professional Crypto Scanner for Explosive Move Detection</h1>
                     <div className='run-button-alignment'>
