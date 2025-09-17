8567caf166cfd6eef6825401cab311a098c22178
parag <pkakabari22@gmail.com>
2025-09-17 23:59:47 +0530

    Comment out handleScan invocation in useEffect to prevent automatic scanning on component mount

--- components/MarketBreakouts/MarketBreakouts.js
+++ components/MarketBreakouts/MarketBreakouts.js
@@ -29,7 +29,7 @@ export default function MarketBreakouts() {
     };
 
     useEffect(() => {
-        handleScan();
+        // handleScan();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
